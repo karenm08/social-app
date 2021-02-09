50842d900572d45d4e3257cd2b45539d45b115f4
karenm08 <wmok14@my.bcit.ca>
2021-02-08 23:25:05 -0800

    added login page

---
+++ src/components/PageHeader/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import PageHeader from './index'
+
+export default {
+    title: 'PageHeader',
+    component: PageHeader,
+  }
+
+  export const Default = () => <PageHeader/>
