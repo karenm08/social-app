15c3061ba948de920736827c5c5f92b049d59ce2
karenm08 <wmok14@my.bcit.ca>
2021-02-21 13:05:47 -0800

    Create a new network.js

---
+++ src/network.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+export async function getPosts() {
+  try {
+    const result = await axios.get(`http://localhost:8080/api/posts`)
+    return result.data
+  } catch (error) {
+    console.log(error)
+    alert(error.message)
+    // We need a better plan for handling errors
+    // So that the user knows what's gone wrong
+    // We can handle this later though
+  }
+}
