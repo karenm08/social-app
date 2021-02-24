be524023f9a359c3594c8152fe8ceed002f35c17
karenm08 <wmok14@my.bcit.ca>
2021-02-24 00:22:41 -0800

    clean up code

--- src/components/NewPost/index.js
+++ src/components/NewPost/index.js
@@ -93,7 +93,6 @@ export default function NewPost({ imgUpload, onSubmit, fileUpload , onClose }) {
                 <Input onChange={event => setImageURL(event.target.value)} className={classes.input} placeholder="Image URL"></Input>
                 <Input onChange={event => setImageURLDes(event.target.value)} className={classes.input} placeholder="Description"></Input>
                 <Button className={classes.postButton} value="post" onClick={() => onSubmit({description: imgDescription, imageUrl:imageURL,type: "url"})}>POST</Button>
-                {/* <Button className={classes.postButton} value="post" onClick={() => onSubmit({description: imgDescription, imageURL:IMGimageURL})}>POST</Button> */}
             </FormControl>
           </Typography>
         ) : (
@@ -102,7 +101,6 @@ export default function NewPost({ imgUpload, onSubmit, fileUpload , onClose }) {
                 <FormControl fullWidth={true}>
                     <Input onChange={event => setFileUploadDes(event.target.value)} className={classes.input} placeholder="Description"></Input>
                     <Button className={classes.postButton} value="post" onClick={() => onSubmit({description: description, file:"", type: "url"})}>POST</Button>
-                    {/* <Button className={classes.postButton} value="post" onClick={() => onSubmit({description: description, file:fileUpload.file})}>POST</Button> */}
                 </FormControl>
           </Typography>
         )}
