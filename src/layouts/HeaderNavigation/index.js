8bed6fde389ef640bed9c014f3404da291620e3b
karenm08 <wmok14@my.bcit.ca>
2021-03-01 00:24:00 -0800

    installed jwt

--- src/layouts/HeaderNavigation/index.js
+++ src/layouts/HeaderNavigation/index.js
@@ -2,13 +2,14 @@ import PageHeader from '../../components/PageHeader'
 
 import { useHistory } from "react-router-dom"
 
-export default function HeaderNavigation() {
+export default function HeaderNavigation({user, setToken}) {
   const history = useHistory()
 
   return (<PageHeader
     title="SocialLOL"
     titleClicked={() => history.push("/")}
-    user={{_id: "1"}}
+    // user={{_id: "1"}}
+    user={user}
     newPost={() => history.push("/newPost")}
     profile={() => history.push("/profile")}
     login={() => history.push("/login")}
